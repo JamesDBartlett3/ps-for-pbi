3792f300323de1576c8e6124eba4ab0f589ddb10
James Bartlett <jbartlett@dmu.edu>
2024-01-04 15:19:39 -0600

    - added OutputFolder param - added OutputFolder example - added a TODO item: add ExtractWithPbiTools switch param - refactored some variable names - moved FileIsBlankPbix function def from process block to begin block

--- Export-PowerBIThinModelsFromWorkspaces.ps1
+++ Export-PowerBIThinModelsFromWorkspaces.ps1
@@ -4,7 +4,7 @@
   
   .DESCRIPTION
 		This script will export one or more "Thin" Models (Power BI Semantic Models with no corresponding Report) from Power BI as PBIX files.
-		It can be used to export a single Thin Model by specifying the DatasetId, WorkspaceId, BlankPbix, and OutFile parameters.
+		It can be used to export a single Thin Model by specifying the DatasetId, WorkspaceId, BlankPbix, and OutputFile parameters.
 		It can also be used to export multiple Thin Models by piping the output of the Get-PowerBIThinModelsFromWorkspaces.ps1 script to it.
   
   .PARAMETER DatasetId
@@ -19,11 +19,14 @@
   .PARAMETER WorkspaceName
     The name of the Workspace containing the Model to export
   
+  .PARAMETER OutputFile
+    Local filename for one exported Model PBIX file
+  
+  .PARAMETER OutputFolder
+    Local folder to store multiple workspace subfolders and exported Model PBIX files
+  
   .PARAMETER BlankPbix
     Path (local or URL) to a blank PBIX file to upload and rebind to the Model to be exported
-  
-  .PARAMETER OutFile
-    Local path to save the Model PBIX file to
   
   .INPUTS
     Selected.System.String (one or more objects with the following property names):
@@ -36,14 +39,19 @@
     This script does not output anything to the pipeline
   
   .EXAMPLE
-    # Export a single Thin Model as a PBIX file by specifying the DatasetId, WorkspaceId, BlankPbix, and OutFile parameters
-    .\Export-PowerBIThinModelsFromWorkspaces.ps1 -DatasetId "00000000-0000-0000-0000-000000000000" -WorkspaceId "00000000-0000-0000-0000-000000000000" -BlankPbix "C:\blank.pbix" -OutFile "C:\new.pbix"
-  
+    # Export a single Thin Model as a PBIX file by specifying the DatasetId, WorkspaceId, BlankPbix, and OutputFile parameters
+    .\Export-PowerBIThinModelsFromWorkspaces.ps1 -DatasetId "00000000-0000-0000-0000-000000000000" -WorkspaceId "00000000-0000-0000-0000-000000000000" -BlankPbix "C:\blank.pbix" -OutputFile "C:\new.pbix"
+
   .EXAMPLE 
     # Use the Get-PowerBIThinModelsFromWorkspaces.ps1 script with the Interactive switch to pick one or more Thin Models,
     # and pipe the output into Export-PowerBIThinModelsFromWorkspaces.ps1 to export the selected Thin Models as PBIX files
     .\Get-PowerBIThinModelsFromWorkspaces.ps1 -Interactive | .\Export-PowerBIThinModelsFromWorkspaces.ps1
   
+  .EXAMPLE
+    # Pipe the output of the Get-PowerBIThinModelsFromWorkspaces.ps1 script to this script, export all detected thin models to which the user has access,
+    # and use the OutputFolder parameter to specify that they should be stored in a folder named "PowerBIThinModels" in the user's Downloads directory
+    .\Get-PowerBIThinModelsFromWorkspaces.ps1 | .\Export-PowerBIThinModelsFromWorkspaces.ps1 -OutputFolder "$env:USERPROFILE\Downloads\PowerBIThinModels"
+
   .NOTES
     This script does NOT require Azure AD app registration, 
     service principal creation, or any other special setup.
@@ -59,6 +67,8 @@
       - Thanks to my wife (@likeawednesday@techhub.social) for her support and encouragement.
       - Thanks to the PowerShell and Power BI/Fabric communities for being so awesome.
       - Thanks to @santisq & @seeminglyscience on PowerShell Discord for their guidance on using a process block to enable streaming inputs from the pipeline.
+
+    TODO: Add ExtractWithPbiTools switch parameter
 	
   .LINK
     [Source code](https://github.com/JamesDBartlett3/ps-for-pbi/blob/main/Export-PowerBIThinModelsFromWorkspaces.ps1)
@@ -85,13 +95,14 @@ Param(
   [Parameter(Mandatory, ValueFromPipelineByPropertyName)][guid]$WorkspaceId,
   [Parameter(ValueFromPipelineByPropertyName)][Alias('Name')][string]$ModelName,
   [Parameter(ValueFromPipelineByPropertyName)][string]$WorkspaceName,
-  [Parameter()][string]$BlankPbix,
-  [Parameter()][string]$OutFile
+  [Parameter()][string]$OutputFolder,
+  [Parameter()][string]$OutputFile,
+  [Parameter()][string]$BlankPbix
 )
 
 begin {
+  [string]$outputDirectory = $OutputFolder ?? (Join-Path -Path $env:TEMP -ChildPath 'PowerBIThinModels')
   [string]$blankPbixUri = 'https://github.com/JamesDBartlett3/PowerBits/raw/main/Misc/blank.pbix'
-  [string]$tempFolder = Join-Path -Path $env:TEMP -ChildPath 'PowerBIThinModels'
   [string]$blankPbixTempFile = Join-Path -Path $env:TEMP -ChildPath 'blank.pbix'
   [string]$pbiApiBaseUri = 'https://api.powerbi.com/v1.0/myorg'
   [string]$urlRegex = '(http[s]?|[s]?ftp[s]?)(:\/\/)([^\s,]+)'
@@ -103,17 +114,7 @@ begin {
   [bool]$localFileIsValid = $false
   [int]$thinModelCount = 0
   [int]$errorCount = 0
-  Invoke-Item -Path $tempFolder
-}
-
-process {
-  
-  Write-Debug "DatasetId: $DatasetId, WorkspaceId: $WorkspaceId, DatasetName: $ModelName, WorkspaceName: $WorkspaceName"
-  
-  $headers = [System.Collections.Generic.Dictionary[[String], [String]]]::New()
-  
-  [string]$uniqueName = 'temp_' + [guid]::NewGuid().ToString().Replace('-', '')
-  
+  Invoke-Item -Path $outputDirectory
   Function FileIsBlankPbix($file) {
     $zip = [System.IO.Compression.ZipFile]::OpenRead($file)
     $fileIsPbix = @($validPbixContents | Where-Object { $zip.Entries.Name -Contains $_ }).Count -gt 0
@@ -128,16 +129,25 @@ process {
       return $false
     }
   }
+}
+
+process {
   
-  # If the temp folder doesn't exist and user has not specified OutFile location, create the temp folder
-  if (!(Test-Path -LiteralPath $tempFolder) -and !$OutFile) {
-    New-Item -Path $tempFolder -ItemType Directory | Out-Null
+  Write-Debug "DatasetId: $DatasetId, WorkspaceId: $WorkspaceId, DatasetName: $ModelName, WorkspaceName: $WorkspaceName"
+  
+  $headers = [System.Collections.Generic.Dictionary[[String], [String]]]::New()
+  
+  [string]$uniqueName = 'temp_' + [guid]::NewGuid().ToString().Replace('-', '')
+  
+  # If the output folder doesn't exist and user has not specified OutputFile location, create the output folder
+  if (!(Test-Path -LiteralPath $outputDirectory) -and !$OutputFile) {
+    New-Item -Path $outputDirectory -ItemType Directory | Out-Null
   }
   
   # If user specified a URL to a file, download and validate it as a blank PBIX file
   if ($blankPbixIsUrl) {
     Write-Verbose "Downloading file: $BlankPbix..."
-    Invoke-WebRequest -Uri $BlankPbix -OutFile $blankPbixTempFile
+    Invoke-WebRequest -Uri $BlankPbix -OutputFile $blankPbixTempFile
     Write-Verbose 'Validating downloaded file...'
     $remoteFileIsValid = FileIsBlankPbix($blankPbixTempFile)
   }
@@ -158,7 +168,7 @@ process {
   # download one from GitHub, and then check if it's valid and blank
   else {
     Write-Verbose "Downloading a blank pbix file from GitHub to $blankPbixTempFile..."
-    Invoke-WebRequest -Uri $blankPbixUri -OutFile $blankPbixTempFile
+    Invoke-WebRequest -Uri $blankPbixUri -OutputFile $blankPbixTempFile
     $defaultFileIsValid = FileIsBlankPbix($blankPbixTempFile)
   }
   
@@ -191,6 +201,9 @@ process {
     }
   }
   
+  # Add Content-Type: application/json to the headers
+  $headers.Add('Content-Type', 'application/json')
+  
   # If user did not specify a Model name, get it from the API
   $ModelName = $ModelName ?? (Get-PowerBIDataset -Id $DatasetId -WorkspaceId $WorkspaceId).Name
   
@@ -206,46 +219,43 @@ process {
   Write-Debug "Published Report ID: $publishedReportId; Published Model ID: $publishedDatasetId"
   
   # Assemble the Workspace base URI
-  $workspaceBaseUri = "$pbiApiBaseUri/groups/$WorkspaceId"
+  [string]$workspaceBaseUri = "$pbiApiBaseUri/groups/$WorkspaceId"
   # Assemble the Datasets API URI
-  $datasetsEndpoint = "$workspaceBaseUri/datasets"
+  [string]$datasetsEndpoint = "$workspaceBaseUri/datasets"
   # Assemble the Reports API URI
-  $reportsEndpoint = "$workspaceBaseUri/reports"
+  [string]$reportsEndpoint = "$workspaceBaseUri/reports"
   # Assemble the Rebind API URI
-  $updateReportContentEndpoint = "$reportsEndpoint/$publishedReportId/Rebind"
-  # Assemble the Rebind API request body
-  $body = "{`"datasetId`": `"$DatasetId`"}"
+  [string]$updateReportContentEndpoint = "$reportsEndpoint/$publishedReportId/Rebind"
   # Assemble the Export API URI
-  $exportEndpoint = "$reportsEndpoint/$publishedReportId/Export"
-  
-  # Add the Content-Type header to the request
-  $headers.Add('Content-Type', 'application/json')
+  [string]$exportEndpoint = "$reportsEndpoint/$publishedReportId/Export"
+  # Assemble the Rebind API request body
+  [string]$body = "{`"datasetId`": `"$DatasetId`"}"
   
   # Rebind the published Report to the Thin Model
   Write-Verbose "Rebinding published Report $publishedReportId to Model $DatasetId..."
   Invoke-RestMethod -Uri $updateReportContentEndpoint -Method POST -Headers $headers -Body $body | Out-Null
   
   # If the Workspace folder doesn't exist, create it
-  if (!(Test-Path (Join-Path -Path $tempFolder -ChildPath $WorkspaceName))) {
-    New-Item -Path (Join-Path -Path $tempFolder -ChildPath $WorkspaceName) -ItemType Directory | Out-Null
+  if (!(Test-Path (Join-Path -Path $outputDirectory -ChildPath $WorkspaceName))) {
+    New-Item -Path (Join-Path -Path $outputDirectory -ChildPath $WorkspaceName) -ItemType Directory | Out-Null
   }
   
-  # If user did not specify an output file name, use the Model's name and save it in the default temp folder
-  $OutFile = if (!!$OutFile) { $OutFile } else {
-    Join-Path -Path $tempFolder -ChildPath (Join-Path -Path $WorkspaceName -ChildPath "$($ModelName).pbix")
+  # If user did not specify an output file name, use the Model's name and save it in the output folder
+  $OutputFile = if (!!$OutputFile) { $OutputFile } else {
+    Join-Path -Path $outputDirectory -ChildPath (Join-Path -Path $WorkspaceName -ChildPath "$($ModelName).pbix")
   }
   
   # Export the re-bound Report and Model (a.k.a. "Thick Report") PBIX file to a temp file first, then 
   # rename it to the correct name (workaround for Models with special characters in their names)
   Write-Verbose "Exporting re-bound blank Report and Model (a.k.a. 'Thick Report') $publishedReportId to temporary file $($uniqueName).pbix..."
-  $tempFileName = Join-Path -Path $tempFolder -ChildPath "$uniqueName.pbix"
+  [string]$tempFileName = Join-Path -Path $outputDirectory -ChildPath "$uniqueName.pbix"
   Invoke-RestMethod -Uri "$exportEndpoint" `
     -Method GET -Headers $headers `
     -ContentType 'application/octet-stream' `
     -Body '{"preferClientRouting":true}' `
     -ErrorVariable message `
     -ErrorAction SilentlyContinue `
-    -OutFile $tempFileName 2>&1 | Out-Null
+    -OutputFile $tempFileName 2>&1 | Out-Null
   if ($message) {
     $errorCount++
     $errorCode = ($message.ErrorRecord.ErrorDetails.Message | ConvertFrom-Json).error.code
@@ -254,10 +264,10 @@ process {
   else {
     $thinModelCount++
     Write-Verbose "Exported Thin Model `"$ModelName`" from `"$WorkspaceName`" to $tempFileName"
-    Write-Verbose "Moving and renaming temp file $($uniqueName).pbix to $OutFile..."
-    Move-Item -Path $tempFileName -Destination $OutFile -Force
+    Write-Verbose "Moving and renaming temp file $($uniqueName).pbix to $OutputFile..."
+    Move-Item -Path $tempFileName -Destination $OutputFile -Force
   }
-  $OutFile = $null
+  $OutputFile = $null
   
   # Delete the blank Report and its original Model from the Workspace
   Write-Verbose "Deleting temporary blank Model $publishedDatasetId and Report $publishedReportId from Workspace $WorkspaceId..."
@@ -267,10 +277,10 @@ process {
 }
 
 end {
-  Write-Verbose "Thin Models successfully exported: $thinModelCount.$(if($errorCount -gt 0){" Errors encountered: $errorCount"})"
+  Write-Verbose "Thin Models successfully exported: $thinModelCount -- $(if($errorCount -gt 0){" Errors encountered: $errorCount"})"
   
   # Remove any empty directories
-  Get-ChildItem $tempFolder -Recurse -Attributes Directory | Where-Object { $_.GetFileSystemInfos().Count -eq 0 } | Remove-Item
+  Get-ChildItem $outputDirectory -Recurse -Attributes Directory | Where-Object { $_.GetFileSystemInfos().Count -eq 0 } | Remove-Item
   
   # Clear the PowerShell session's memory
   [gc]::Collect()
