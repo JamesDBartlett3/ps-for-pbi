2bcfcad86d15276f60b0c75293c1d6a94010289b
JamesDBartlett3 <37491308+JamesDBartlett3@users.noreply.github.com>
2024-01-09 12:32:53 -0600

    added OpenFile parameter

--- Checkpoint-WorkspaceUsageMetrics.ps1
+++ Checkpoint-WorkspaceUsageMetrics.ps1
@@ -10,16 +10,19 @@
   
   .PARAMETER UsageMetric
     The usage metric to export. Defaults to 'Report views'.
-
+  
   .PARAMETER OutFile
     The path to the CSV file to export the usage metrics to. Defaults to a file in the user's temp directory.
-
+  
+  .PARAMETER OpenFile
+    Open the CSV file after it has been exported.
+  
   .EXAMPLE
     # Exports usage metrics for a workspace in Power BI
     .\Checkpoint-WorkspaceUsageMetrics.ps1 -WorkspaceID 12345678-1234-1234-1234-123456789012
   
   .NOTES
-		Warning: This script uses an internal API that is not officially supported by Microsoft. It may break at any time. Use at your own risk.
+    Warning: This script uses an internal API that is not officially supported by Microsoft. It may break at any time. Use at your own risk.
     TODO: Add support for Excel
   
   .LINK
@@ -39,9 +42,10 @@
 #>
 
 Param(
-	[Parameter(Mandatory = $false)][guid]$WorkspaceID,
-	[Parameter(Mandatory = $false)][string]$UsageMetric = 'Report views',
-	[Parameter(Mandatory = $false)][string]$OutFile
+  [Parameter(Mandatory = $false)][guid]$WorkspaceID,
+  [Parameter(Mandatory = $false)][string]$UsageMetric = 'Report views',
+  [Parameter(Mandatory = $false)][string]$OutFile,
+  [Parameter(Mandatory = $false)][switch]$OpenFile
 )
 
 # PowerShell dependencies
@@ -50,86 +54,87 @@ Param(
 $headers = [System.Collections.Generic.Dictionary[[String], [String]]]::New()
 
 try {
-	$headers = Get-PowerBIAccessToken
+  $headers = Get-PowerBIAccessToken
 }
 
 catch {
-	Write-Host '🔒 Power BI Access Token required. Launching Azure Active Directory authentication dialog...'
-	Start-Sleep -s 1
-	Connect-PowerBIServiceAccount -WarningAction SilentlyContinue | Out-Null
-	$headers = Get-PowerBIAccessToken
-	if ($headers) {
-		Write-Host '🔑 Power BI Access Token acquired. Proceeding...'
-	}
-	else {
-		Write-Host '❌ Power BI Access Token not acquired. Exiting...'
-		Exit
-	}
+  Write-Host '🔒 Power BI Access Token required. Launching Azure Active Directory authentication dialog...'
+  Start-Sleep -s 1
+  Connect-PowerBIServiceAccount -WarningAction SilentlyContinue | Out-Null
+  $headers = Get-PowerBIAccessToken
+  if ($headers) {
+    Write-Host '🔑 Power BI Access Token acquired. Proceeding...'
+  }
+  else {
+    Write-Host '❌ Power BI Access Token not acquired. Exiting...'
+    Exit
+  }
 }
 
 $token = $headers['Authorization']
 
 # Workspace ID
 if (!$WorkspaceID) {
-	$WorkspaceID = Read-Host -Prompt 'Enter the workspace ID'
+  $WorkspaceID = Read-Host -Prompt 'Enter the workspace ID'
 }
 
 # a function that detects the URI of PowerBI the cluster where the workspace is currently located
 function Get-PowerBiApiClusterUri() {
-	$reply = Invoke-RestMethod -Uri 'https://api.powerbi.com/v1.0/myorg/datasets' -Headers @{ 'Authorization' = $token } -Method GET
-	$unaltered = $reply.'@odata.context'
-	$stripped = $unaltered.split('/')[2]
-	$clusterURI = "https://$stripped/beta/myorg/groups"
-	return $clusterURI
+  $reply = Invoke-RestMethod -Uri 'https://api.powerbi.com/v1.0/myorg/datasets' -Headers @{ 'Authorization' = $token } -Method GET
+  $unaltered = $reply.'@odata.context'
+  $stripped = $unaltered.split('/')[2]
+  $clusterURI = "https://$stripped/beta/myorg/groups"
+  return $clusterURI
 }
 
 function Get-WorkspaceUsageMetrics($wid, $UsageMetric) {
-	$psRequestBody = @{
-		queries            = @(
-			@{
-				query = "EVALUATE '" + $UsageMetric + "'"
-			}
-		)
-		serializerSettings = @{
-			includeNulls = $false
-		}
-	}
-	$url = Get-PowerBiApiClusterUri
-	$data = Invoke-WebRequest -Uri "$url/$wid/usageMetricsReportV2?experience=power-bi" -Headers @{ 'Authorization' = $token }
-	$response = $data.Content.ToString().Replace('nextRefreshTime', 'NextRefreshTime').Replace('lastRefreshTime', 'LastRefreshTime') | ConvertFrom-Json
-	$dmname = $response.models[0].dbName
-	$publicEndpoint = "https://api.powerbi.com/v1.0/myorg/groups/$wid/datasets/$dmname/executeQueries"
-	$result = Invoke-PowerBIRestMethod -Method POST -Url $publicEndpoint -Body ($psRequestBody | ConvertTo-Json)
-	$jsonResult = $result | ConvertFrom-Json
-	$rows = $jsonResult.results[0].tables[0].rows
-	return $rows
+  $psRequestBody = @{
+    queries            = @(
+      @{
+        query = "EVALUATE '" + $UsageMetric + "'"
+      }
+    )
+    serializerSettings = @{
+      includeNulls = $false
+    }
+  }
+  $url = Get-PowerBiApiClusterUri
+  $data = Invoke-WebRequest -Uri "$url/$wid/usageMetricsReportV2?experience=power-bi" -Headers @{ 'Authorization' = $token }
+  $response = $data.Content.ToString().Replace('nextRefreshTime', 'NextRefreshTime').Replace('lastRefreshTime', 'LastRefreshTime') | ConvertFrom-Json
+  $dmname = $response.models[0].dbName
+  $publicEndpoint = "https://api.powerbi.com/v1.0/myorg/groups/$wid/datasets/$dmname/executeQueries"
+  $result = Invoke-PowerBIRestMethod -Method POST -Url $publicEndpoint -Body ($psRequestBody | ConvertTo-Json)
+  $jsonResult = $result | ConvertFrom-Json
+  $rows = $jsonResult.results[0].tables[0].rows
+  return $rows
 }
 
 $result = Get-WorkspaceUsageMetrics -wid $WorkspaceID -UsageMetric $UsageMetric
 
 if (!$result) {
-	Write-Host "No usage metrics found for workspace $WorkspaceID"
-	Exit
+  Write-Host "No usage metrics found for workspace $WorkspaceID"
+  Exit
 }
 
 $columnNames = $result[0].PSObject.Properties.Name
 $newColumnNames = $columnNames.ForEach({ $_.Replace($UsageMetric, '').Replace("[", "").Replace("]", "") })
 
 for ($i = 0; $i -lt $columnNames.length; $i++) {
-	$result | Add-Member -MemberType AliasProperty -Name $newColumnNames[$i] -Value $columnNames[$i]
+  $result | Add-Member -MemberType AliasProperty -Name $newColumnNames[$i] -Value $columnNames[$i]
 }
 
 $timestamp = Get-Date -Format 'yyyy-MM-dd_HH-mm-ss'
 
 $resultPath = if ( !$OutFile -or $OutFile -notlike "*.csv" ) {
-	$workspaceName = (Get-PowerBIWorkspace -Id $WorkspaceID).Name
-	Join-Path -Path $env:TEMP `
-		-ChildPath "$($workspaceName.Replace(' ',''))_$($UsageMetric.Replace(' ',''))_$($timestamp).csv"
+  $workspaceName = (Get-PowerBIWorkspace -Id $WorkspaceID).Name
+  Join-Path -Path $env:TEMP `
+    -ChildPath "$($workspaceName.Replace(' ',''))_$($UsageMetric.Replace(' ',''))_$($timestamp).csv"
 }
 else {
-	$OutFile
+  $OutFile
 }
 
 $result | Select-Object -Property $newColumnNames | Export-Csv -Path $resultPath
 Write-Host "Saving file to $resultPath"
-Invoke-Item $resultPath
+
+if ($OpenFile) { Invoke-Item $resultPath }
