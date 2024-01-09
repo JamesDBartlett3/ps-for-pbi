d4f95c5841b809623c6b457276487a7f4f08aad7
JamesDBartlett3 <37491308+JamesDBartlett3@users.noreply.github.com>
2024-01-09 12:33:03 -0600

    Update Power BI scripts and add new scripts for exporting data and managing workspaces

--- _PBIFabricSummit2024Demo.ps1
+++ _PBIFabricSummit2024Demo.ps1
@@ -1,34 +1,41 @@
-# Checkpoint-PowerBIScannerApiData.ps1
+# Export Power BI Scanner API data to a JSON file, then open it
 .\Checkpoint-PowerBIScannerApiData.ps1 -OpenFile
 
-# Checkpoint-PowerBIWorkspaceSecurity.ps1
-.\Checkpoint-PowerBIWorkspaceSecurity.ps1
+# Export security settings for all Power BI Workspaces to an Excel file, then open it
+.\Checkpoint-PowerBIWorkspaceSecurity.ps1 -OpenFile
 
-# Checkpoint-WorkspaceUsageMetrics.ps1
-.\Checkpoint-WorkspaceUsageMetrics.ps1 `
+# Export usage metrics for a Power BI Workspace to a CSV file, then open it
+.\Checkpoint-WorkspaceUsageMetrics.ps1 -OpenFile `
 	-WorkspaceID 12398b8b-568b-4191-8af3-968475609e40
 
-# Copy-GoalInScorecard.ps1
+# Create one or more copies of a Goal on a Power BI Scorecard
+# This is useful for quickly creating multiple Goals that are similar to an existing one
 .\Copy-GoalInScoreCard.ps1
 
-# Copy-PowerBIReportContentToBlankPBIXFile.ps1
+# Copy the content of a Power BI Report to a blank PBIX file, then download that file
+# This is useful for downloading a report that was authored in a personal workspace,
+# or is otherwise not available for download
 .\Copy-PowerBIReportContentToBlankPBIXFile.ps1 `
-	-SourceReportId  `
-	-SourceWorkspaceId 
+	-SourceReportId `
+	-SourceWorkspaceId
 
-# Update-PowerBIReportDatasetBinding.ps1
+# Rebind a Power BI Report to a different dataset
 .\Update-PowerBIReportDatasetBinding.ps1 `
 	-TargetDatasetID c542b17f-e941-45b4-938f-c70545abb9b3 `
 	-ReportWorkspaceID 80500cbf-25ce-4b64-b191-5273c111d617 `
 	-ReportID 7f96ade7-7875-48bd-a112-5d9e0feaf869
 
-# Working with Reports and Models
-.\Export-PowerBIReportsFromWorkspaces.ps1 `
-	-ExtractWithPbiTools `
-	-SkipExistingFiles `
-	-ThrottleLimit 4
+# Now change it back
+.\Update-PowerBIReportDatasetBinding.ps1 `
+	-TargetDatasetID 273fc7bc-703d-4106-b591-5f89a32420a8 `
+	-ReportWorkspaceID 80500cbf-25ce-4b64-b191-5273c111d617 `
+	-ReportID 7f96ade7-7875-48bd-a112-5d9e0feaf869
 
-.\Update-PowerBIReportDatasetBinding.ps1
+# Export Power BI Reports from Workspaces, and extract their source code with PBI-Tools
+.\Export-PowerBIReportsFromWorkspaces.ps1 -ExtractWithPbiTools
 
-# Thin Models
+# Get a list of Thin Models from selected Workspaces, and export them to a temp folder
 .\Get-PowerBIThinModelsFromWorkspaces.ps1 -Interactive | .\Export-PowerBIThinModelsFromWorkspaces.ps1
+
+# Get the status of all Data Gateway nodes to which the current user has access
+.\Get-DataGatewayStatus.ps1
