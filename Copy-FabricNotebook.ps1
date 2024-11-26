e494db086b63791f360bf783c56c9e21c97f0d89
JamesDBartlett3 <37491308+JamesDBartlett3@users.noreply.github.com>
2024-11-26 10:09:10 -0600

    Add Copy-FabricNotebook.ps1 script to copy notebooks between workspaces

---
+++ Copy-FabricNotebook.ps1
@@ -0,0 +1,36 @@
+<#
+  .SYNOPSIS
+    Copies a Fabric notebook from one workspace to another.
+  
+  .DESCRIPTION
+  
+
+  .PARAMETER SourceWorkspace
+
+  .PARAMETER Notebook
+
+  .PARAMETER TargetWorkspace
+
+  .EXAMPLE
+    
+  
+  .NOTES
+    ACKNOWLEDGEMENTS:
+      - Thanks to my wife (@likeawednesday@techhub.social) for her support and encouragement.
+      - Thanks to the PowerShell and Power BI/Fabric communities for being so awesome.
+  
+  .LINK
+    [Source code](https://github.com/JamesDBartlett3/ps-for-pbi/blob/main/Copy-FabricNotebook.ps1)
+  
+  .LINK
+    [The author's blog](https://datavolume.xyz)
+  
+  .LINK
+    [Follow the author on LinkedIn](https://www.linkedin.com/in/jamesdbartlett3/)
+  
+  .LINK
+    [Follow the author on Mastodon](https://techhub.social/@JamesDBartlett3)
+  
+  .LINK
+    [Follow the author on BlueSky](https://bsky.app/profile/jamesdbartlett3.bsky.social)
+#>
